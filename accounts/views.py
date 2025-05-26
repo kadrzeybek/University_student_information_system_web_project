b3bfb53ee1527545d5010a24eea4c3e49a6690d8
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-05-26 23:44:23 +0300

    gradespage front done

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,7 @@ def login_view(request):
                 return redirect('/student/homepage')  # students/urls.py'da name='profil'
             elif user.role == "instructor":
                 request.session['instructor_id'] = user.instructor_id
-                return redirect('/instructor/course-program')  # Örnek: instructor ana sayfası
+                return redirect('/instructor/grades')  # Örnek: instructor ana sayfası
             else:
                 pass
 
--- instructors/urls.py
+++ instructors/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('profile/', views.profile_view, name='instructor_profile'),
     path('my-courses/', views.courses_view, name='instructor_my_courses'),
     path('course-program/', views.course_program_view, name='instructor_course_program'),
+    path('grades/', views.grades_view, name='instructor_grades'),
 ]
