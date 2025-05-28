21e751dc1f7f55f1b69d96ae4c6d0d29779a843e
Doğukan Özçelik <dogukanozcelik673@gmail.com>
2025-05-29 00:46:19 +0300

    static files path configured

--- USI_system/settings.py
+++ USI_system/settings.py
@@ -44,9 +44,6 @@ CSRF_TRUSTED_ORIGINS = [
     'https://universitystudentinformationsystemwebprojec-production.up.railway.app'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -60,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'accounts.middleware.AuthenticationMiddleware',  # Middleware'in burada olduğundan emin olun
+    'accounts.middleware.AuthenticationMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'USI_system.urls'
@@ -145,10 +144,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
